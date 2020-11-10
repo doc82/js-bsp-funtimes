c0abe4c920ef1c7966e3ecd699d677d4bd6f7667
Bruce Bjorklund <bjorklund.bruce@gmail.com>
2020-11-10 14:32:37 -0800

    blah

--- src/bsp-node.js
+++ src/bsp-node.js
@@ -1,5 +1,3 @@
-// # class Node
-
 // Holds a node in a BSP tree. A BSP tree is built from a collection of polygons
 // by picking a polygon to split along. That polygon (and all other coplanar
 // polygons) are added directly to that node and the other polygons are added to
